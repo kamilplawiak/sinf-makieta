bf17da1c4122a695d0a2bfffb20e8200213842d3
Pławiak Kamil <AK78852@akmf.pl>
2023-03-23 09:49:30 +0100

    remove unnecessary code

--- src/app/menu.service.ts
+++ src/app/menu.service.ts
@@ -7,23 +7,6 @@ import { NavigationModel } from "./navigation.model";
     providedIn: 'root'
 })
 export class MenuService {
-    #simpleItems = [
-        { title: 'Podmiot', content: 'Wyszukaj dane finansowe po NIP.' },
-        { title: 'Osoba fizyczna', content: 'Wyszukaj dane finansowe po PESEL.' },
-        { title: 'Rachunek', content: 'Wyszukaj dane finansowe po numerze rachunku.' }
-    ];
-
-    #complexItems = [
-        { title: 'Podmiot', content: 'Wyszukaj dane finansowe, jeżeli nie posiadasz NIP.' },
-        { title: 'Osoba fizyczna', content: 'Wyszukaj dane finansowe, jeżeli nie posiadasz PESEL.' }
-    ];
-
-    #searchItems = [
-        { title: 'Podmiot', content: 'Wyszukaj dane finansowe po NIP.' },
-        { title: 'Osoba fizyczna', content: 'Wyszukaj dane finansowe po PESEL.' },
-        { title: 'Rachunek', content: 'Wyszukaj dane finansowe po numerze rachunku.' }
-    ]
-
     #userItems = new Map();
     #username = '';
     #activeIndex = new Subject<number>();
